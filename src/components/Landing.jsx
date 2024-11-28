55659896a1c55d9a036be6b199d843d8c4452177
dasartini <adriansartinid@gmail.com>
2024-11-28 21:27:08 +0000

    footer idea

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,15 @@
 import LandingStyle from "../styles/LandingStyle"
-
+import chicago from '../assets/images/chicago.jpg'
 function Landing(){
 
     return(
   <main>
 <LandingStyle>
         <div class="search-section">
-      <h2 class="section-title">Ricerca per musei</h2>
+      <h2 class="section-title">Search by museum</h2>
       <div class="museum-cards">
         <div class="card">
-          <img src="https://cdn.choosechicago.com/uploads/2019/10/AIC_Facade-900x600.png" alt="Uffizi" class="card-image"/>
+          <img src={chicago} alt="Art Institute of Chicago " class="card-image"/>
           <h3 class="card-title">Art Institute of Chicago</h3>
         </div>
         <div class="card">
