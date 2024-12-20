467be7774790c68601e0e3358d2bd5f230656971
dasartini <adriansartinid@gmail.com>
2024-12-20 18:33:06 +0000

    refactor 2

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -37,13 +37,13 @@ function Slideshow() {
               alt={artwork.title}
               title={artwork.title}
             />
-   <div className="badgecont">
-  <img className="badge" src={badge} alt="Badge" />
-  <div className="textCont">
-    <p className="badgeTitle">{artwork.title}</p>
-    <p className="badgeAuthor">{artwork.artist}</p>
-  </div>
-</div>
+            <div className="badgecont">
+              <img className="badge" src={badge} alt="Badge" />
+              <div className="textCont">
+                <p className={artwork.title.length > 20 ? "badgeTitle" : "badgeTitleShort"}>{artwork.title}</p>
+                <p className={artwork.title.length > 20 ? "badgeAuthor" : "badgeAuthorShort "}>{artwork.artist}</p>
+              </div>
+            </div>
 
 
           </div>
