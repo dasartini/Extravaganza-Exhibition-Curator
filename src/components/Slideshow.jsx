adf3dd6f9f7d3956938c5f8a672b5f3bcefdfaf6
dasartini <adriansartinid@gmail.com>
2024-12-21 18:02:39 +0000

    working

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -37,13 +37,13 @@ function Slideshow() {
               alt={artwork.title}
               title={artwork.title}
             />
-            <div className="badgecont">
+            {/* <div className="badgecont">
               <img className="badge" src={badge} alt="Badge" />
               <div className="textCont">
                 <p className={artwork.title.length > 20 ? "badgeTitle" : "badgeTitleShort"}>{artwork.title}</p>
                 <p className={artwork.title.length > 20 ? "badgeAuthor" : "badgeAuthorShort "}>{artwork.artist}</p>
               </div>
-            </div>
+            </div> */}
 
 
           </div>
