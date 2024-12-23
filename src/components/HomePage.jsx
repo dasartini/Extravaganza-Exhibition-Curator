b25342159d0ea58e96cfddf387a97563ef80b52f
dasartini <adriansartinid@gmail.com>
2024-12-23 01:27:39 +0000

    finished

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,38 +1,73 @@
 import HomePageStyle from "../styles/HomePageStyle";
 import image from "../assets/images/homepageImg.jpg";
-import {Link } from "react-router";
+import { Link } from "react-router";
 
-function HomePage(){
+function HomePage() {
 
-    return(
-   
-   <>
-   <HomePageStyle>
-    <div className="homePageCont">
-        <h1> EXTRAVAGANZA</h1>
-        <br></br>
-        <div className="quote">
-        <img  className="quoteImg"src={image}/>
-        <h3>"<b>Art cannot be criticised because every mistake is a new creation.</b>" – Mr. Brainwash</h3></div>
-        <br></br>
-        <div className="homePageText">
-        According to the Cambridge dictionary, <i>extravaganza</i> means:<br></br> <br></br>
-        <i>"a large, exciting, and expensive event or entertainment"</i><br></br>      
-          <p>For me, as an art lover, <i>Extravaganza</i> means something amazing – something that represents an escape from routine.
-          Something remarkable, capable of leaving a lasting impact on the collective mind. </p><br></br>
-        <p>Taking art and history as inspiration, this web application allows you to explore amazing artworks from famous museums worldwide.</p>
-        <p>Here, you will discover many renowned artists who have left their mark on the world. But you will also encounter artists whose names you may not recognise, yet whose works are equally extraordinary.</p><br></br>
-        <p> Feel free to navigate between the different museums available. You can also add artworks to your personal gallery and reorder them however you like.</p>
-        <p>And the best part? You can show your gallery with friends in a slideshow.</p>
-        <br></br>
-        <p>Enjoy exploring the ways different souls express themselves. </p>
+  return (
+    <>
+      <HomePageStyle>
+        <div className="homePageCont" role="main" aria-labelledby="main-heading">
+          <h1 id="main-heading">EXTRAVAGANZA</h1>
+          <br />
+          <div className="quote" aria-labelledby="quote-heading">
+            <img
+              className="quoteImg"
+              src={image}
+              alt="A vibrant artistic representation"
+            />
+            <h3 id="quote-heading">
+              "<b>Art cannot be criticised because every mistake is a new creation.</b>" – Mr. Brainwash
+            </h3>
+          </div>
+          <br />
+          <div
+            className="homePageText"
+            aria-labelledby="definition-heading"
+            role="region"
+          >
+            <h2 id="definition-heading">Definition of Extravaganza</h2>
+            <p>
+              According to the Cambridge dictionary, <i>extravaganza</i> means:
+            </p>
+            <blockquote>
+              <i>"a large, exciting, and expensive event or entertainment"</i>
+            </blockquote> <br></br>
+            <p>
+              For me, as an art lover, <i>Extravaganza</i> means something amazing – something that represents an escape
+              from routine. Something remarkable, capable of leaving a lasting impact on the collective mind.
+            </p>
+            <p>
+              Taking art and history as inspiration, this web application allows you to explore amazing artworks from
+              famous museums worldwide.
+            </p>
+            <p>
+              Here, you will discover many renowned artists who have left their mark on the world. But you will also
+              encounter artists whose names you may not recognise, yet whose works are equally extraordinary.
+            </p>
+            <p>
+              Feel free to navigate between the different museums available. You can also add artworks to your personal
+              gallery and reorder them however you like.
+            </p>
+            <p>And the best part? You can show your gallery with friends in a slideshow.</p> <br></br>
+            <p>Enjoy exploring the ways different souls express themselves.</p> <br></br>
+          </div>
+          <br />
+          <mark
+            style={{ background: "#000", color: "#fff" }}
+            role="button"
+            tabIndex={0}
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            aria-label="Explore the museums"
+          >
+            <Link to={"/museums"}>
+              <h3>Explore the museums</h3>
+            </Link>
+          </mark>
         </div>
-        <br></br><br></br>
-        <mark onClick={window.scrollTo({ top: 0, behavior: "smooth" })} style={{background:"#000", color: "#fff"}}> <Link  to={'/museums'}> <h3>Explore the museums</h3></Link> </mark>
-    </div>
-    </HomePageStyle>
+      </HomePageStyle>
     </>
-    )
+  );
 
 }
 
